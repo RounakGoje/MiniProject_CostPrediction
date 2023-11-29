9004433ecac02b4474d20b289d0dbefd4cb7a2cc
RounakGoje <67039381+RounakGoje@users.noreply.github.com>
2023-11-29 15:16:59 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,505 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+ 
+<link rel="stylesheet" type="text/css" href="css3/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css3/style.css">
+  <link rel="stylesheet" type="text/css" href="css3/registerform.css">
+    
+  <style>
+    .land-header{
+      margin: 80px 0;
+    }
+    .test {
+      width: 100px;
+      height: 100px;
+      background-color: #f8f8f8;
+    }
+    .features{
+       background: #f8f8f8;
+    }
+    .features p{
+      font-size: 1rem;
+      
+    }
+    .testpeople {
+      margin: 5rem;
+    }
+    .ff{
+      padding-top: 1.5rem;
+    }
+    /* .fa{
+      font-size: 3rem;
+      color: #2FA4E7
+    } */
+
+    @media only screen and (max-width: 600px)  {
+      .header{
+              height: 70vh;
+            }
+                 
+      .fa-sm-2x{
+        font-size: 2.33em;
+      }
+      .extra-div h2{
+        font-size: 0.8rem;
+      }
+    }
+   </style>
+</head>
+<body id="bootstrap-overrides">
+<div class="header" id="topheader">
+	
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    
+ 
+
+
+<div class="container text-uppercase p-2">
+
+  <a class="navbar-brand" href="http://localhost/p/miniproject/landding page/index.php" id="about">Car Spot</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto text-uppercase">
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/p/miniproject/about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/p/miniproject/clientlogin.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/p/miniproject/clientregform.php">Registration</a>
+      </li>
+
+    </ul>
+
+
+<!-- ------------------------------ -->
+    <form action="http://localhost/p/miniproject/index.php" method="post" class="form-inline col-md-4 my-2 my-lg-0 pull-right d-none d-lg-block" >
+    
+   <div class="row mx-3 ">
+       <input class="form-control col-md-8 mr-sm-2" name="search"type="search" placeholder="Your Services" aria-label="Search">
+
+        <button class="btn btn-success  my-2 my-sm-0" name="submit" type="submit">Search</button>
+   </div>
+     
+   
+    </form>
+    <!-- ------------------ -->
+
+  </div>
+  </div>
+</nav>
+
+<section class="header-section">
+  <div class="center-div">
+    <h1 class="font-weight-bold">Welcome</h1>
+    <p>................</p>  
+      <a href="#">About</a>
+      <a href="#">contact</a>
+
+    </div>
+    
+  </div>
+  
+</section>
+
+  </div><!--end of header-->
+	
+
+
+
+ 
+
+<!--*************************************header end*************************************** -->
+
+
+<!--*************************three extra header div start*********************8 -->
+
+
+<section class="header-extradiv land-header" >
+  <div class="container">
+    <h4 class="font-weight-normal pb-5">Why Car Spot Web</h4>
+    <div class="row">
+      <div class=" extra-div  col-4 my-2">
+        <a href="#"><i  class="fa-3x fa-sm-2x fa fa-desktop" aria-hidden="true"></i></a>
+<h2>EASY TO USE</h2>
+<p class="d-none d-md-block">10,000+ fullu inspected cars online</p>
+      </div>
+   <div class=" extra-div col-4 my-2">
+        <a href="#"><i  class="fa-3x fa-sm-2x fa fa-magic" aria-hidden="true"></i></a>
+<h2>Take a test drive at your home </h2>
+<p class="d-none d-md-block">Sanitized cars for every test drive</p>
+      </div>      
+            <div class=" extra-div col-4 my-2">
+        <a href="#"><i  class="fa-3x fa-sm-2x fa fa-magic" aria-hidden="true"></i></a>
+<h2>EASY TO GET SERVICES</h2>
+<p class="d-none d-md-block">And 5-day money back gaurantee </p>
+      </div>      
+
+    </div><!--end of row-1 -->
+    
+  </div>
+</section>
+
+
+      
+          
+    
+<!--*************************try start********************-- ***--> 
+
+    <div style="background: rgb(245,233,174);
+background: linear-gradient(180deg, rgba(245,233,174,0.8130602582830007) 0%, rgba(210,178,17,0.6898109585631127) 100%);">
+  
+    <h2 style=  "text-align: center"  >All About Servicio</h2 >
+    </div>
+    <section style="background: rgb(245,233,174);
+background: linear-gradient(180deg, rgba(245,233,174,0.404096672848827) 0%, rgba(210,178,17,0.28644961402529767) 100%);">
+										<p style="background: rgb(245,233,174);
+background: linear-gradient(180deg, rgba(245,233,174,0.404096672848827) 0%, rgba(210,178,17,0.28644961402529767) 100%);"><span class="image left"><img src="images3/f.jpg" alt="" />
+                                            </span><br>The Car Spot is an Marketing Platflorm Where Customers can Directly Deal with Dealers.<br>
+                                            No Need to Serach Here and There if Car Spot is Here.<br><br>
+                                            Car Spot is an Efficient website which can provide the all Information of different cars and many more.<br> <br> Also the customer can register their details.<br>
+                                             Details of Cars their information easily from anywhere at anytime.
+                                            <br><br>
+                                            The Website gives the Reliable Environment to every customer.<br>
+
+Clients can compare various service providers for a same service.<br>
+                                            The WebsiTte is Designed to Perform Several Tasks. <br>
+Many of the client users and service providers can access the Website at a Time.<br>
+The Client, User and Service Providers can easily access the website by just Clicking on Links.<br><br>
+        </p>
+										
+    
+    </section>
+                 
+    
+<!-- =========================
+    FAQ SECTION   
+============================== -->
+<section  style="background:linear-gradient(to right , #1e5799 0%,#3ccdbb 0%, #16c9f6 100%); "id="faq" class="parallax-section">
+	<div class="container">
+		<div class="row">
+	<!-- Section title
+			================================================== -->
+			<div class="wow bounceIn col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 text-center">
+				<div class="section-title">
+					<h2>Do you have Questions?</h2>
+					<p>Car Spot Has Friendly Enviroment with Fun Elements And A Easy Userinterface.</p>
+				</div>
+			</div>
+
+			<div class="wow fadeInUp col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10" data-wow-delay="0.9s">
+				<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+  					<div class="panel panel-default">
+   						<div class="panel-heading" role="tab" id="headingOne">
+      						<h4 class="panel-title">
+        						<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+          							 Is it a Free Platform  ?
+        						</a>
+      						</h4>
+    					</div>
+   						<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+      						<div class="panel-body">
+        						<p>Yes Totaly.</p>
+								<p>Car Spot gives the Free Reliable Environment to every customer.</p>
+      						</div>
+   						 </div>
+ 					</div>
+
+    				<div class="panel panel-default">
+   						<div class="panel-heading" role="tab" id="headingTwo">
+      						<h4 class="panel-title">
+        						<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          							Can we search different cars?
+        						</a>
+      						</h4>
+    					</div>
+   						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+      						<div class="panel-body">
+                            	<p>Yes . </p>
+        						<p>you can add Your Own Service.<br>
+      						</div>
+   						 </div>
+ 					</div>
+
+ 					<div class="panel panel-default">
+   						<div class="panel-heading" role="tab" id="headingThree">
+      						<h4 class="panel-title">
+        						<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          							What Things Car Spot Provides?
+        						</a>
+      						</h4>
+    					</div>
+   						<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+      						<div class="panel-body">
+                                <p>Car Spot Provides :- </p>
+                            	<p>Car Spot Provides Friendly Enviroment with easy UserInterface.<br>
+                                Customers can compare various service Cars.</p>
+      						</div>
+   						 </div>
+ 					 </div>
+
+ 				 </div>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+
+    
+         <!--*************************try1 start********************--> 
+    
+        <div style="background: rgb(245,233,174);
+background: linear-gradient(180deg, rgba(245,233,174,0.8130602582830007) 0%, rgba(210,178,17,0.6898109585631127) 100%);">
+    <h1 style="text-align: center">How Car Spot Works </h1>
+</div>
+    
+   <section id="venue" class="parallax-section">
+	<div class="container">
+		<div class="row">
+              &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
+
+			
+			<div class="container2">
+          
+        <div class="card2">
+            <div class="face face1">
+                <div class="content2">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                    <h3>Book Your Test Drive</h3>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content2">
+                    <p>As For Your Requirement and Need, Book Your Test Drive.</p>
+                        <a style="color: red" >Checkout Next Step -></a>
+                </div>
+            </div>
+        </div>
+        <div class="card2">
+            <div class="face face1">
+                <div class="content2">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true">
+                    <h3>Search</h3>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content2">
+                    <p>As Per Your Requirement and Need, Search You Dream Car. </p>
+                        <a style="color: red" >Checkout Next Step -></a>
+                </div>
+            </div>
+        </div>
+        <div class="card2">
+            <div class="face face1">
+                <div class="content2">
+                    <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true">
+                    <h3>Contact</h3>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content2">
+                    <p>But Your Dream Car at Car Spot As Per Your Requirement.</p>
+                        <a style="color: red"  >!!Thank-You!! </a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+          	
+            		
+
+		</div>
+        	
+    </div>
+</section>
+          
+   
+    
+    
+    
+    
+    
+    
+    
+   
+     <!--*************************try2 end********************-->     
+    
+    
+    
+    
+    
+ 
+   
+              <!--*************************try3 strat********************--  
+    
+      <div class="box3">
+      <div class="card3">
+        <div class="imgBx">
+            <img src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="images">
+        </div>
+        <div class="details">
+            <h2>SomeOne Famous<br><span>Director</span></h2>
+        </div>
+      </div>
+    
+       <div class="card3">
+         <div class="imgBx">
+            <img src="https://images.unsplash.com/photo-1549417229-aa67d3263c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="images">
+         </div>
+         <div class="details">
+            <h2>SomeOne Famous<br><span>Producer</span></h2>
+          </div>
+       </div>
+
+       <div class="card3">
+         <div class="imgBx">
+            <img src="https://images.unsplash.com/photo-1548094878-84ced0f6896d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="images">
+         </div>
+         <div class="details">
+            <h2>SomeOne Famous<br><span>Actor</span></h2>
+          </div>
+       </div>
+ 
+  </div>
+
+         
+
+      <!--*************************try5 start****************
+    
+    
+	<div class="image-area">
+		<div class="img-wrapper">
+			<img src="https://images.pexels.com/photos/1220757/pexels-photo-1220757.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Atul Prajapati">
+			<h2>David james</h2>
+			<ul>
+				<li><a href="https://github.com/atuljustano"><i class="fab fa-github"></i></a></li>
+				<li><a href="https://www.instagram.com/atulkprajapati2000/"><i class="fab fa-instagram"></i></a></li>
+				<li><a href="https://twitter.com/atuljustano"><i class="fab fa-twitter"></i></a></li>
+				<li><a href="https://www.youtube.com/channel/UCf-KfxuY8PZBSD_8RW2nYsw"><i class="fab fa-youtube"></i></a></li>
+			</ul>
+		</div>
+	</div>
+    ****--> 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+<!--*************************footer start********************-->
+
+
+<footer class="ff" >
+  <p>
+    <div class="container text-center">
+      <h2 class="mt-0">Direct Links</h2>
+    
+      <button type="button" class="btn btn-secondary">ADMIN</button>
+      <button type="button" class="btn btn-success">FEEDBACK </button>
+      <button type="button" class="btn btn-info">LOGIN</button>
+      <button type="button" class="btn btn-warning">CONTACT-US</button>
+    </div>
+</p>
+
+      <div class="footer-container" >
+        <div class="left-col"  >
+          
+  
+          <p  class="rights-text" style="color: #ab4197" ><b >@THANK-YOU FOR VISITING I HOPE YOU ENJOYED...</b> </p>
+          <a href="#" class="fa fa-facebook"></a>
+          <a href="#" class="fa fa-twitter"></a>
+          <a href="#" class="fa fa-google"></a>
+          <a href="#" class="fa fa-linkedin"></a>
+          <a href="#" class="fa fa-youtube"></a>
+          <a href="#" class="fa fa-instagram"></a>
+          <a href="#" class="fa fa-pinterest"></a>
+          <a href="#" class="fa fa-snapchat-ghost"></a>
+          <a href="#" class="fa fa-skype"></a>
+        </div>
+
+    <br>
+    <br>
+    <br>
+    <br>
+</footer>
+
+
+
+
+<!--*************************footer end********************-->
+
+
+
+
+          
+
+          
+          
+          
+  
+          
+          
+          
+          
+          
+          
+
+
+
+
+ <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+    
+
+
+
+
+
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js" integrity="sha256-jDnOKIOq2KNsQZTcBTEnsp76FnfMEttF6AV2DF2fFNE="crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-up/1.0.0/jquery.counterup.min.js" integrity="sha256-JtQPj/3xub8oapVMaIijPNoM0DHoAtgh/gwFYuN5rik="crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+<script>
+  $('.count').counterUp({
+    delay:10,
+    time:8000
+  })
+
+</script>
+
+
+
+</body>
+</html>
