91ff14b1163d1a824ee363beca742d1fa64a58ea
RounakGoje <67039381+RounakGoje@users.noreply.github.com>
2023-11-29 20:36:38 +0530

    Add files via upload

---
+++ landding page/index.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Car Spot Home Page</title>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+ 
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/registerform.css">
+    
+
+  <style>
+    .land-header{
+      margin: 80px 0;
+    }
+    .test {
+      width: 100px;
+      height: 100px;
+      background-color: #f8f8f8;
+    }
+    .features{
+       background: #f8f8f8;
+    }
+    .features p{
+      font-size: 1rem;
+      
+    }
+    .testpeople {
+      margin: 5rem;
+    }
+    .ff{
+      padding-top: 1.5rem;
+    }
+    /* .fa{
+      font-size: 3rem;
+      color: #2FA4E7
+    } */
+
+    @media only screen and (max-width: 600px)  {
+      .header{
+              height: 70vh;
+            }
+                 
+      .fa-sm-2x{
+        font-size: 2.33em;
+      }
+      .extra-div h2{
+        font-size: 0.8rem;
+      }
+    }
+   </style>
+</head>
+<body id="bootstrap-overrides">
+<div class="header" id="topheader">
+	
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    
+ 
+
+
+<div class="container text-uppercase p-2">
+
+  <a class="navbar-brand" href="index.php" id="about">Car Spot</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto text-uppercase">
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/p/miniproject/about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/p/miniproject/clientlogin.php">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/p/miniproject/clientregform.php">Registration</a>
+      </li>
+
+    </ul>
+
+<!-- ------------------------------ -->
+    <form action="http://localhost/p/miniproject/index.php" method="post" class="form-inline col-md-4 my-2 my-lg-0 pull-right d-none d-lg-block" >
+    
+   <div class="row mx-3 ">
+   	   <input class="form-control col-md-8 mr-sm-2" name="search"type="search" placeholder="Your Services" aria-label="Search">
+
+   	    <button class="btn btn-success  my-2 my-sm-0" name="submit" type="submit">Search</button>
+
+   </div>
+     
+   
+    </form>
+    <!-- ------------------ -->
+
+  </div>
+  </div>
+</nav>
+
+<section class="header-section">
+  <div class="center-div">
+    <h1 class="font-weight-bold">Welcome</h1>
+    <p>................</p>  
+      <a href="#">About</a>
+      <a href="#">contact</a>
+
+    </div>
+    
+  </div>
+  
+</section>
+
+  </div><!--end of header-->
+	
+
+
+
+ 
+
+<!--*************************************header end*************************************** -->
+
+
+<!--*************************three extra header div start*********************8 -->
+
+
+<section class="header-extradiv land-header" >
+  <div class="container">
+    <h4 class="font-weight-normal pb-5">Why Car Spot Web</h4>
+    <div class="row">
+      <div class=" extra-div  col-4 my-2">
+        <a href="#"><i  class="fa-3x fa-sm-2x fa fa-desktop" aria-hidden="true"></i></a>
+<h2>EASY TO USE</h2>
+<p class="d-none d-md-block">10,000+ fullu inspected cars online</p>
+      </div>
+   <div class=" extra-div col-4 my-2">
+        <a href="#"><i  class="fa-3x fa-sm-2x fa fa-magic" aria-hidden="true"></i></a>
+<h2>Take a test drive at your home </h2>
+<p class="d-none d-md-block">Sanitized cars for every test drive</p>
+      </div>      
+            <div class=" extra-div col-4 my-2">
+        <a href="#"><i  class="fa-3x fa-sm-2x fa fa-magic" aria-hidden="true"></i></a>
+<h2>Online Payment & Doorstep Delivery</h2>
+<p class="d-none d-md-block">And 5-day money back gaurantee </p>
+      </div>
+
+
+
+
+      .
+
+
+    </div><!--end of row-1 -->
+    
+  </div>
+</section>
+
+ <!--*************************try1 start********************-->     
+     
+<div style="background-color: blanchedalmond">
+<h1 style="text-align: center">How Car Spot Works </h1>
+</div>
+     <div class="container2">
+          
+        <div class="card2">
+            <div class="face face1">
+                <div class="content2">
+                    <a href="http://localhost/p/miniproject/transaction.php"><img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true">
+                    <h3>Book Your Test Drive</h3></a>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content2">
+                    <a href="http://localhost/p/miniproject/transaction.php"><p>As For Your Requirement and Need, Book Your Test Drive.</p></a>
+                        <a style="color: red" href="http://localhost/p/miniproject/transaction.php">Checkout Next Step -></a>
+                </div>
+            </div>
+        </div>
+        <div class="card2">
+            <div class="face face1">
+                <div class="content2">
+                    <a href="http://localhost/p/miniproject/searchpage.php"><img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"></a>
+                    <a href="http://localhost/p/miniproject/searchpage.php"><h3>Search</h3></a>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content2">
+                    <a href="http://localhost/p/miniproject/searchpage.php"><p>As Per Your Requirement and Need, Search You Dream Car. </p></a>
+                        <a style="color: red" href="http://localhost/p/miniproject/searchpage.php">Checkout Next Step -></a> 
+                </div>
+            </div>
+        </div>
+        <div class="card2">
+            <div class="face face1">
+                <div class="content2">
+                    <a href="http://localhost/p/miniproject/contact.php"><img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true">
+                    <h3>Contact</h3></a>
+                </div>
+            </div>
+            <div class="face face2">
+                <div class="content2">
+                    <a href="http://localhost/p/miniproject/contact.php"><p>But Your Dream Car at Car Spot As Per Your Requirement.</p></a>
+                        <a style="color: red" href="">!!Thank-You!! </a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+          
+       
+    
+<!--*************************footer start********************-->
+
+
+<footer class="ff" >
+  <p>
+    <div class="container text-center">
+      <h2 class="mt-0">Direct Links</h2>
+      <button type="button" class="btn btn-primary">ABOUT</button>
+      <button type="button" class="btn btn-secondary">ADMIN</button>
+      <button type="button" class="btn btn-success">FEEDBACK </button>
+      <button type="button" class="btn btn-info">LOGIN</button>
+      <button type="button" class="btn btn-warning">CONTACT-US</button>
+    </div>
+</p>
+      <div class="footer-container">
+        <div class="left-col" >
+          
+  
+          <p  class="rights-text" style="color: #ab4197" ><b>@THANK-YOU FOR VISITING I HOPE YOU ENJOYED...</b> </p>
+          <a href="#" class="fa fa-facebook"></a>
+          <a href="#" class="fa fa-twitter"></a>
+          <a href="#" class="fa fa-google"></a>
+          <a href="#" class="fa fa-linkedin"></a>
+          <a href="#" class="fa fa-youtube"></a>
+          <a href="#" class="fa fa-instagram"></a>
+          <a href="#" class="fa fa-pinterest"></a>
+          <a href="#" class="fa fa-snapchat-ghost"></a>
+          <a href="#" class="fa fa-skype"></a>
+        </div>
+
+        <div class="right-col">
+          <h1>Our Newsletter</h1>
+          <div class="border"></div>
+          <p style="color:#ab4197"><B>Enter Your Email to get our news and updates.</B></p>
+          <form action="" class="newsletter-form">
+            <input type="text" class="txtb" placeholder="Enter Your Email" name="email">
+            <input type="submit" class="btn" value="Submit">
+          </form>
+        </div>
+      </div>
+</footer>
+
+
+
+
+<!--*************************footer end********************-->
+
+
+
+
+          
+
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+
+
+
+
+ <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+    
+
+
+
+
+
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js" integrity="sha256-jDnOKIOq2KNsQZTcBTEnsp76FnfMEttF6AV2DF2fFNE="crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-up/1.0.0/jquery.counterup.min.js" integrity="sha256-JtQPj/3xub8oapVMaIijPNoM0DHoAtgh/gwFYuN5rik="crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+<script>
+  $('.count').counterUp({
+    delay:10,
+    time:8000
+  })
+
+</script>
+
+
+
+</body>
+</html>
